991c2c6682aef72a1d63eebb8d9719b4398260fd
Caio Felipe <cfxd99@gmail.com>
2021-10-26 19:30:17 -0300

    adicionado rota do perfil do usuário

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
 import { Get3LastMessagesController } from "./controllers/GetLast3MessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
 import { ensuredAuthenticated } from "./middleware/ensuredAuthenticated";
 
 const router = Router();
@@ -15,5 +16,10 @@ router.post(
 );
 
 router.get("/messages/last3", new Get3LastMessagesController().handle);
+router.get(
+	"/profile",
+	ensuredAuthenticated,
+	new ProfileUserController().handle
+);
 
 export { router };
